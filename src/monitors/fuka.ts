5f1b9460750583f35958546e8395ee0c012b56b9
Youssef Habri <youssef.habri@gmail.com>
2018-11-14 14:47:59 +0100

    Added a timeout for the `fuka` command

--- src/monitors/fuka.ts
+++ src/monitors/fuka.ts
@@ -1,11 +1,12 @@
 import { KlasaMessage, Monitor } from 'klasa';
 
 export default class FukaBotMonitor extends Monitor {
+  private timeout: number;
   private fukaID = '510000124949168165';
   private responses: string[] = [
     `<@${this.fukaID}>, Is that all you can do. Pff!`,
     `Oh, wow. <@${this.fukaID}>, you're actually trying!`,
-    `Ehm, I too can add numbers. But, I don't wanna take <@${this.fukaID}>'s only job. You should be grateful.`
+    `Ehm, I too can add numbers. But, I don't wanna take <@${this.fukaID}>'s only job. You should be grateful.`,
   ];
 
   constructor(...args) {
@@ -19,23 +20,28 @@ export default class FukaBotMonitor extends Monitor {
       ignoreWebhooks: true,
       ignoreEdits: true,
       ignoreBlacklistedUsers: true,
-      ignoreBlacklistedGuilds: true
+      ignoreBlacklistedGuilds: true,
     });
+    this.timeout = new Date().getTime();
   }
 
   async run(message: KlasaMessage) {
     const n = Math.floor(Math.random() * this.responses.length);
-    if (message.author.id === this.fukaID) {
+    if (message.author.id === '139360031102599168' && (this.timeout <= new Date().getTime())) {
 
+      // Delay the command & start typing so it looks natural
       message.channel.startTyping();
       await this.delay(1500);
       message.channel.stopTyping();
+
+      // Set a timeout for the command so it doesn't become annoying (10 minutes)
+      this.timeout = new Date().getTime() + (10 * 60 * 1000);
 
       return message.send(this.responses[n]);
     }
   }
 
   async delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 }
